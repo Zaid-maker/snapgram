f001a5cc70ba84e7511fdc31ac0b17973d87650f
Zaid-maker <pzhafeez@gmail.com>
2023-10-28 02:16:40 +0500

    config intellisense

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [react()],
 });
