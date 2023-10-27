1a09502770d1669febdee3982bc4b37b6e387465
Zaid-maker <pzhafeez@gmail.com>
2023-10-28 00:48:59 +0500

    update the routes and fixes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import "./globals.css";
 
 import { Routes, Route } from "react-router-dom";
 
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import SignupForm from "@/_auth/forms/SignupForm";
+import SigninForm from "@/_auth/forms/SigninForm";
+import { Home } from "@/_root/pages";
+
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* Public Routes */}
-        <Route path="/sign-in" element={<SigninForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
         {/* Private Routes */}
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   );
