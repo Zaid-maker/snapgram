cd102b7ff47443aa322bf4d23138ce8c732715f7
Zaid-maker <pzhafeez@gmail.com>
2023-10-28 00:20:16 +0500

    update App to include Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./globals.css";
 
+import { Routes, Route } from "react-router-dom";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+
+        {/* Private Routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
