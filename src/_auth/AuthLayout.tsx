fc2be76b5faa22eea55199f346357dcbddb1f932
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-10-28 16:00:05 +0000

    update authlayout

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -8,9 +8,17 @@ const AuthLayout = () => {
       {isConnected ? (
         <Navigate to="/" />
       ) : (
-        <section>
-          <Outlet />
-        </section>
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 objext-cover bg-no-repeat"
+          />
+        </>
       )}
     </>
   );
