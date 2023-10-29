14f537f439b335df946e346c09daf008def12754
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-10-29 14:51:57 +0000

    update the form

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -14,6 +14,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { SignupValidation } from "@/lib/validation";
 import Loader from "@/components/shared/Loader";
+import { Link } from "react-router-dom";
 
 const SignupForm = () => {
   const isLoading = false;
@@ -114,6 +115,15 @@ const SignupForm = () => {
               "Sign Up"
             )}
           </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account?
+            <Link
+              to="/sign-in"
+              className="text-primary-500 text-small-semibold ml-2"
+            >
+              Log in
+            </Link>
+          </p>
         </form>
       </div>
     </Form>
