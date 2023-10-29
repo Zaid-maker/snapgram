e3bac742979c1b85fb949bca3b95b667bd14cbe1
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-10-29 15:15:16 +0000

    update appwrite config

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,3 +1,16 @@
+import { Account, Avatars, Client, Databases, Storage } from "appwrite"
+
 export const appwriteConfig = {
-    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+    url: import.meta.env.VITE_APPWRITE_URL
 }
+
+export const client = new Client()
+
+client.setProject(appwriteConfig.projectId)
+client.setEndpoint(appwriteConfig.url)
+
+export const account = new Account(client)
+export const databases = new Databases(client)
+export const storage = new Storage(client)
+export const avatars = new Avatars(client)
