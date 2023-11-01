7c0ab3d0f907a1c667cc2ae30287aec066b6358c
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-01 19:01:32 +0000

    pass QueryProvider in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { QueryProvider } from "./lib/react-query/QueryProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <App />
+    <QueryProvider>
+      <App />
+    </QueryProvider>
   </BrowserRouter>
 );
