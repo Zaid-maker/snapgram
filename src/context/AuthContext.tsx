fd65df2f0c311636e2690ae210acbc895cc45191
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-02 08:26:25 +0000

    update the context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,5 +1,54 @@
-const AuthContext = () => {
-  return <div>AuthContext</div>;
+import { IUser } from "@/types";
+import React, { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const INITIAL_USER = {
+  id: "",
+  name: "",
+  username: "",
+  email: "",
+  imageUrl: "",
+  bio: "",
 };
 
-export default AuthContext;
+const INITIAL_STATE = {
+  user: INITIAL_USER,
+  isLoading: false,
+  isAuthenticated: false,
+  setUser: () => {},
+  setIsAuthenticated: () => {},
+  checkAuthUser: async () => false as boolean,
+};
+
+type IContextType = {
+  user: IUser;
+  isLoading: boolean;
+  setUser: React.Dispatch<React.SetStateAction<IUser>>;
+  isAuthenticated: boolean;
+  setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
+  checkAuthUser: () => Promise<boolean>;
+};
+
+const AuthContext = createContext<IContextType>(INITIAL_STATE);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  // const navigate = useNavigate();
+  const [user, setUser] = useState<IUser>(INITIAL_USER);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const checkAuthUser = async () => {
+    setIsLoading(true);
+  };
+
+  const value = {
+    user,
+    setUser,
+    isLoading,
+    isAuthenticated,
+    setIsAuthenticated,
+    checkAuthUser,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
