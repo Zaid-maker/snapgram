109812f6286fe1f749ca78ae0f2bd37577e20e06
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-03 19:56:07 +0000

    fix

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,21 +1,21 @@
-import { Account, Avatars, Client, Databases, Storage } from "appwrite"
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
 
 export const appwriteConfig = {
-    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
     url: import.meta.env.VITE_APPWRITE_URL,
+    projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
     databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
     storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
-    userCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
-    postCollectionId: import.meta.env.VITE_APPWRITE_POSTS_COLLECTION_ID,
-    saveCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID
-}
+    userCollectionId: import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID,
+    postCollectionId: import.meta.env.VITE_APPWRITE_POST_COLLECTION_ID,
+    savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
+};
 
-export const client = new Client()
+export const client = new Client();
 
-client.setProject(appwriteConfig.projectId)
-client.setEndpoint(appwriteConfig.url)
+client.setEndpoint(appwriteConfig.url);
+client.setProject(appwriteConfig.projectId);
 
-export const account = new Account(client)
-export const databases = new Databases(client)
-export const storage = new Storage(client)
-export const avatars = new Avatars(client)
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const avatars = new Avatars(client);
