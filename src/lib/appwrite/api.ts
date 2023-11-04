e0915d8c71c9d3617f942e87a56a63bbcae4c73c
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-11-04 18:41:50 +0000

    ran prettier command

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -3,91 +3,91 @@ import { ID, Query } from "appwrite";
 import { account, appwriteConfig, avatars, databases } from "./config";
 
 export async function createUserAccount(user: INewUser) {
-    try {
-        const newAccount = await account.create(
-            ID.unique(),
-            user.email,
-            user.password,
-            user.name
-        );
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      user.email,
+      user.password,
+      user.name
+    );
 
-        if (!newAccount) throw Error;
+    if (!newAccount) throw Error;
 
-        const avatarUrl = avatars.getInitials(user.name);
+    const avatarUrl = avatars.getInitials(user.name);
 
-        const newUser = await saveUserToDB({
-            accountId: newAccount.$id,
-            name: newAccount.name,
-            email: newAccount.email,
-            username: user.username,
-            imageUrl: avatarUrl,
-        });
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
 
-        return newUser;
-    } catch (error) {
-        console.log(error);
-        return error;
-    }
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
 
 export async function signInAccount(user: { email: string; password: string }) {
-    try {
-        const session = await account.createEmailSession(user.email, user.password);
+  try {
+    const session = await account.createEmailSession(user.email, user.password);
 
-        return session;
-    } catch (error) {
-        console.log(error);
-    }
+    return session;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function getAccount() {
-    try {
-        const currentAccount = await account.get();
+  try {
+    const currentAccount = await account.get();
 
-        return currentAccount;
-    } catch (error) {
-        console.log(error);
-    }
+    return currentAccount;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function getCurrentUser() {
-    try {
-        const currentAccount = await getAccount();
+  try {
+    const currentAccount = await getAccount();
 
-        if (!currentAccount) throw Error;
+    if (!currentAccount) throw Error;
 
-        const currentUser = await databases.listDocuments(
-            appwriteConfig.databaseId,
-            appwriteConfig.userCollectionId,
-            [Query.equal("accountId", currentAccount.$id)]
-        );
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("accountId", currentAccount.$id)]
+    );
 
-        if (!currentUser) throw Error;
+    if (!currentUser) throw Error;
 
-        return currentUser.documents[0];
-    } catch (error) {
-        console.log(error);
-        return null;
-    }
+    return currentUser.documents[0];
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 }
 
 export async function saveUserToDB(user: {
-    accountId: string;
-    email: string;
-    name: string;
-    imageUrl: URL;
-    username?: string;
+  accountId: string;
+  email: string;
+  name: string;
+  imageUrl: URL;
+  username?: string;
 }) {
-    try {
-        const newUser = await databases.createDocument(
-            appwriteConfig.databaseId,
-            appwriteConfig.userCollectionId,
-            ID.unique(),
-            user
-        );
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user
+    );
 
-        return newUser;
-    } catch (error) {
-        console.log(error);
-    }
+    return newUser;
+  } catch (error) {
+    console.log(error);
+  }
 }
