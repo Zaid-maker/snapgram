fac1c9892f0a9811c8da8d4e0f01143dd14111bf
DevMirza <pzhafeez@gmail.com>
2024-01-14 13:25:13 +0000

    format

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="flex-center w-full">
+    <div className="w-full flex-center">
       <img src="/assets/icons/loader.svg" alt="loader" width={24} height={24} />
     </div>
   );
